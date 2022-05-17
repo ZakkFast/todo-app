db6020ab48bc799108163925c81b4d1d81442d38
Zakk Fast <zakkfastpro@gmail.com>
2022-05-17 03:53:43 -0700

    created models

---
+++ models/Todo.js
@@ -0,0 +1,35 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Todo extends Model {}
+
+Todo.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    todo_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    due_date: {
+      type: DataTypes.DATEONLY,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'todo',  
+  }
+);
+
+module.exports = Todo
