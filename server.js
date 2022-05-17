b25ad6aadc497bf1149418d279e841cc90262165
Zakk Fast <zakkfastpro@gmail.com>
2022-05-17 10:02:20 -0700

    correct path import

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const sequelize = require('./config/connection');
+const path = require('path')
 
 const routes = require('./routes');
 
