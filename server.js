63e345da60f1e88ff7b5c28066b1312d3b701277
Zakk Fast <zakkfastpro@gmail.com>
2022-05-17 03:39:13 -0700

    created server and db connection

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
-// testing
+const express = require('express');
+const cors = require('cors');
+const sequelize = require('./config/connection');
+
+// const routes = require('./routes');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}! DB connected..`);
+  });
+});
