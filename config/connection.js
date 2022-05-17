f4be9356d4f426fb539b36daf0b160ea0c2765eb
Zakk Fast <zakkfastpro@gmail.com>
2022-05-17 09:53:18 -0700

    heroku is dumb..

--- config/connection.js
+++ config/connection.js
@@ -1,23 +1,19 @@
 require('dotenv').config();
 const Sequelize = require('sequelize');
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
-      port: 5432,
-      dialect: 'postgres',
-      logging: false,
-      dialectOptions: {
-        useUTC: false,
-        dateStrings: true,
-        typeCast: function (field, next) {
-          if (field.type === 'DATETIME') {
-            return field.string();
-          }
-          return next();
-        },
-      },
-    });
+const sequelize = new Sequelize({
+  database: "dbm8c8be0cedsb",
+  username: "luspcykhnocszl",
+  password: "a45e261a4f588e7ca9121a084a43cb2dbcd148025f28d35b42c61c3da32e2588",
+  host: "ec2-44-194-117-205.compute-1.amazonaws.com",
+  port: 5432,
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
+});
 
 module.exports = sequelize;
