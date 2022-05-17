e18f1329ab69c915e66319a1fc45473692729c3f
Zakk Fast <zakkfastpro@gmail.com>
2022-05-17 04:38:54 -0700

    set up routes to create and get tags

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const sequelize = require('./config/connection');
 
-// const routes = require('./routes');
+const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.use(routes);
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => {
