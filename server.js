05b0aeba858abe5bda330789d5dec592dcdc1fb9
Zakk Fast <zakkfastpro@gmail.com>
2022-05-17 10:06:15 -0700

    here we go again..

--- server.js
+++ server.js
@@ -16,9 +16,13 @@ app.use(routes);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => {
