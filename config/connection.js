794465069c55d01908c0026672b42265374b2b27
Zakk Fast <zakkfastpro@gmail.com>
2022-05-17 09:11:51 -0700

    removed JAWZ. Heroku Postgres set up

--- config/connection.js
+++ config/connection.js
@@ -1,23 +1,26 @@
 require('dotenv').config();
 const Sequelize = require('sequelize');
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
-      port: 5432,
-      dialect: 'postgres',
-      logging: false,
-      dialectOptions: {
-        useUTC: false,
-        dateStrings: true,
-        typeCast: function (field, next) {
-          if (field.type === 'DATETIME') {
-            return field.string();
-          }
-          return next();
-        },
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PW,
+  {
+    host: 'localhost',
+    port: 5432,
+    dialect: 'postgres',
+    logging: false,
+    dialectOptions: {
+      useUTC: false,
+      dateStrings: true,
+      typeCast: function (field, next) {
+        if (field.type === 'DATETIME') {
+          return field.string();
+        }
+        return next();
       },
-    });
+    },
+  }
+);
 
 module.exports = sequelize;
